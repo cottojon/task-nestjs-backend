abf2ffd6c84cf8e0c791f73d4a6b5d16d1d3e2dd
cottojon <cottojonathan@ymail.com>
2019-07-26 11:31:50 -0400

    create task module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule); //set Appmpoule as root module
+  await app.listen(3000); //listen in port 300
 }
-bootstrap();
+bootstrap(); //bootstrap application
