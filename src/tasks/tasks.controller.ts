cab30187eeba8b200a7e84b47f957e306619efe3
Jonathan Cotto <cottojonathan@ymail.com>
2019-07-31 14:56:40 -0400

    getTask() added for filtered task searching

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,43 +1,44 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './tasks.model'; 
+import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
+//import dto
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
 
     constructor(private taskService: TasksService) { }
 
-    @Get()
-    getAllTask(): Task[] {
+    @Get()// gonna retrieve these parameters from the query which will be parsed into an object called filterDto 
+    getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+        // if parameters are empty get all task/ if not filter our task
+        if(Object.keys(filterDto).length){
+            return this.taskService.getTasksWithFilters(filterDto);
+        }
+        // keep get all task in the service if the query parameters are empty
         return this.taskService.getAllTask();
     }
 
-   
-    @Post() 
-    createTask(@Body() createTaskDto: CreateTaskDTO): Task { 
-        
-        return this.taskService.createTask(createTaskDto); 
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDTO): Task {
+
+        return this.taskService.createTask(createTaskDto);
     }
 
-
- 
-    @Get('/:id') 
-    getTaskByID(@Param('id') id: string): Task{ 
+    @Get('/:id')
+    getTaskByID(@Param('id') id: string): Task {
         return this.taskService.getTaskByID(id);
     }
 
-
     @Delete('/:id')
-    deleteTaskByID(@Param('id') id: string): void{
+    deleteTaskByID(@Param('id') id: string): void {
         this.taskService.deleteTaskByID(id);
     }
 
-    //our patch handler
-    @Patch('/:id/status') //our url with a id passed in 
+    @Patch('/:id/status') 
     updateTaskStatusByID(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
-        return this.taskService.updateTaskStatus(id, status); //update task
+        return this.taskService.updateTaskStatus(id, status);
     }
-
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,57 +1,67 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
-import * as uuid from 'uuid/v1'; 
+import * as uuid from 'uuid/v1';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];
-
 
     getAllTask(): Task[] {
         return this.tasks;
     }
 
+    // our filter method
+    getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
 
-    createTask(createTaskDto: CreateTaskDTO): Task { 
-        const {title, description} = createTaskDto;
+        // destruct object
+        const { status, search } = filterDto;
+        // get all task
+        let tasks = this.getAllTask();
 
+        if (status) {
+            tasks = tasks.filter(task => task.status === status); // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+        }
+
+        if (search) {
+            tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
+        }
+
+        return tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDTO): Task {
+        const { title, description } = createTaskDto;
 
         const task: Task = {
-            id: uuid(), 
+            id: uuid(),
             title,
             description,
             status: TaskStatus.OPEN,
-        }
+        };
 
-       
         this.tasks.push(task);
 
-       
         return task;
     }
 
-
-    getTaskByID(id: string){
-        return this.tasks.find(task => { 
+    getTaskByID(id: string) {
+        return this.tasks.find(task => {
             return task.id === id;
-        })
+        });
 
     }
 
-
-    deleteTaskByID(id: string): void{ 
-        this.tasks = this.tasks.filter( task => { 
+    deleteTaskByID(id: string): void {
+        this.tasks = this.tasks.filter(task => {
             return task.id !== id;
         });
 
     }
 
-
-    //our new method
     updateTaskStatus(id: string, status: TaskStatus): Task {
-        //find task by id
         const task = this.getTaskByID(id);
-        task.status = status; //update status
+        task.status = status;
         return task;
     }
 
