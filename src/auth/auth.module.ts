dbf1c05b672d8659f3654503ec7cee5d1d88cf4d
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 10:19:02 -0400

    User Repo created and injected inside of auth.service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
-@Module({
+@Module({ //import User Repo for dependency injection in the auth module
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
