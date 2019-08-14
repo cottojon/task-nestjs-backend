04b1ecbd693c3b22852fbb7efe4793a29b578634
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 10:05:17 -0400

    Auth module created with our cnotoller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 // for root= for our root mofule
 // pass in our config
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule], //import module
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule], //import module
 })
 export class AppModule {}
