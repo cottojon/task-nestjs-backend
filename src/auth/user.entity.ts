009f308bb0f37fd69aa21f6bad46c628ec889695
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-15 12:43:32 -0400

    hashed the password in signUp method and added ValidatePassword method

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,7 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import * as brcrypt from 'bcrypt';
+// import bcrypt
+
 
 @Entity()
 @Unique(['username']) // expects an array of column names
@@ -11,4 +14,16 @@ export class User extends BaseEntity{
 
     @Column()
     password: string;
+
+
+    @Column()
+    salt: string;
+
+    // custom method to run business logic for a specific instance of a user
+    // return true if password is correct
+    async validatePassword(password: string): Promise<boolean>{
+        const hash = await brcrypt.hash(password, this.salt);
+        // evaulate to true if this is a match and client side password was correct
+        return hash  === this.password;
+    }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,8 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
-import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-
+import * as brcrypt from 'bcrypt'; // import bcrypt
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
@@ -10,13 +10,23 @@ export class UserRepository extends Repository<User>{
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
+        //generate salt
+        // these are async operations and take some time to compute, 
+        // bcrypt however does provide us with sync methods aswell
+        // the salt will always generate a unique salt for us
+        const salt = await brcrypt.genSalt();
+
+        //create user
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = salt;
+        // hash the password and salt 
+        user.password = await this.hashPassword(password, salt);
+
 
         //try the save query
         try {
-            await user.save();
+             await user.save();
 
         } catch (error) {
             // catch our duplicate username typeorm error code 23505 = duplicate values
@@ -26,5 +36,28 @@ export class UserRepository extends Repository<User>{
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    // if the password is valid return username for now
+    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string>{
+        const {username, password} = authCredentialsDto;
+
+        // check database for the unique username existence
+        const user = await this.findOne({username});
+
+        // check for user existence then call validatePassword to check is password is correct
+        if(user && await user.validatePassword(password)){
+            return user.username; // return username
+        }else{
+            return null;
+        }
+
+    }
+
+
+    // hashing password and salt using bcrypt 
+    // even though our password can be the same since our salts are unique our hashes are always different ;)
+    private async hashPassword(password: string, salt: string): Promise<string>{
+        return brcrypt.hash(password, salt);
     }
 }
