f42d99ae0340a915102ca29bde704df75d20edfc
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-19 09:51:20 -0400

    provide jwt strategy and export jwtstrategy and passport module for the task module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { PassportModule } from '@nestjs/passport';
     }),
     TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy], // add jwt strategy
+  exports: [JwtStrategy, PassportModule] // add to exports so the task module can use it to guard crud operations
 })
 export class AuthModule { }
